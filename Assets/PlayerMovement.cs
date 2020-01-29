19ffff2ab71428f811f207c713e3743e81bf132c
Quinny18 <Callum_Quinn1@outlook.com>
2020-01-29 22:10:28 +0000

    Attempted to Add Existing Unity Project
    Basic Movement completed.
    Added Free Asset pack for prototyping.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float MoveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 Movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement.x = Input.GetAxisRaw("Horizontal");
+        Movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", Movement.x);
+        animator.SetFloat("Vertical", Movement.y);
+        animator.SetFloat("Speed", Movement.sqrMagnitude);
+       
+    }
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + Movement * MoveSpeed * Time.fixedDeltaTime);
+
+        
+
+
+    }
+}
