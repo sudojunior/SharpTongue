585796db54891125139b44b9fec56235557c686d
Quinny18 <Callum_Quinn1@outlook.com>
2020-02-04 16:57:17 +0000

    Added enemy patrolling.

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,41 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Pathfinding;
 
 public class EnemyAI : MonoBehaviour
 {
-
-    public Transform target;
     public float speed = 200f;
     public float nextWaypointDistance = 3f;
 
-    public Transform enemyGFX;
+    private Transform target;
+    private Path path;
+    private int currentWaypoint = 0;
+    private Seeker seeker;
+    private Rigidbody2D rb;
 
-    Path path;
-    int currentWaypoint = 0;
-    bool reachedEndOfPath = false;
-
-    Seeker seeker;
-    Rigidbody2D rb;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-
+        target = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
         InvokeRepeating("UpdatePath", 0f, 0.5f);
-        
     }
 
-    void UpdatePath()
+    private void UpdatePath()
     {
         if (seeker.IsDone())
+        {
             seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
     }
 
-    void OnPathComplete(Path p )
+    private void OnPathComplete(Path p)
     {
         if (!p.error)
         {
@@ -44,21 +37,13 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-
-
-    // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (path == null)
-            return;
-        if(currentWaypoint >= path.vectorPath.Count)
+        if (path == null) { return; }
+
+        if (currentWaypoint >= path.vectorPath.Count)
         {
-            reachedEndOfPath = true;
             return;
-        }
-        else
-        {
-            reachedEndOfPath = false;
         }
 
         Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
@@ -72,6 +57,5 @@ public class EnemyAI : MonoBehaviour
         {
             currentWaypoint++;
         }
-
     }
 }
