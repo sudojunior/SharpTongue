8862b8b687452bf148cb51e49171e533042849e2
Tarik Sabin <tarik.sabin@gmail.com>
2020-02-06 13:23:09 +0000

    Added my health bar stuff. Happy to help integrate it into combat if needed.

---
+++ Assets/HealthStuff/HealthHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthHandler : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthBar;
+    public float playerHealth;
+
+    private void Start()
+    {
+        if (playerHealth == 0)
+        {
+            playerHealth = 100f;
+        }
+    }
+
+    private void Update()
+    {
+        // if hit
+        if (playerHealth > 0)
+        {
+            playerHealth -= 1f;
+            healthBar.SetHealth(playerHealth);
+        }
+        // else dead
+    }
+}
