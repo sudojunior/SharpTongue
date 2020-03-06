587a4c363384921452593db3f3ca07582cfda329
Callum Quinn <Callum_Quinn1@outlook.com>
2020-03-06 11:45:18 +0000

    Added the ranged attack for the player on MB2. Fixed range attacking. Thanks to Harry Donovan, Matt Shaw

--- Assets/PlayerAttackOnMouse.cs
+++ Assets/PlayerAttackOnMouse.cs
@@ -5,21 +5,46 @@ using UnityEngine;
 public class PlayerAttackOnMouse : MonoBehaviour
 {
     public GameObject attackPrefab;
+    public GameObject rangedAttackPrefab;
+    public GameObject player;
     public Transform firePoint;
+    public Camera cam;
+    public Vector3 point = new Vector3();
+    public Vector2 mousePos = new Vector2();
+    public Rigidbody2D rb;
+    public float boltSpeed = 20f;
+
 
     void Update()
     {
-        //Converting camera point to world point
 
         if (Input.GetButtonDown("Fire1"))
         {
             Attack();
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            rangedAttack();
         }
     }
     void Attack()
     {
         GameObject attackObj = Instantiate(attackPrefab, firePoint.position, firePoint.rotation);
         Destroy(attackObj, 1.0f);
+    }
+
+    
+    void rangedAttack()
+    {
+        Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y));
+        Debug.Log(point);
+        GameObject attackObj = Instantiate(rangedAttackPrefab, firePoint.position, firePoint.rotation);
+
+        rb = attackObj.GetComponent<Rigidbody2D>();
+        //Thanks to Matt Shaw for help with this line
+        rb.AddForce((firePoint.transform.position - player.transform.position ).normalized * boltSpeed, ForceMode2D.Impulse);
+            
     }
 
 }
--- Assets/RotateFirePoint.cs
+++ Assets/RotateFirePoint.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class RotateFirePoint : MonoBehaviour
 {
-
+    //Thanks to Harry Donovan for help with fixing this script
     public Camera cam;
-    // public Rigidbody2D rotateFirePointrb;
-    public Transform firePointRotate;
-    Vector2 mousePos;
+
 
     [SerializeField] 
     private float radius;
@@ -16,28 +14,14 @@ public class RotateFirePoint : MonoBehaviour
 
     void Start()
     {
-        pivot = firePointRotate.transform;
-        transform.parent = pivot;
-        transform.position += Vector3.up * radius;
+        pivot = transform.parent;
     }
 
     void FixedUpdate()
     {
-
-        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-
-        //Vector2 lookDir = mousePos - rotateFirePointrb.position;
-
-        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        //rotateFirePointrb.rotation = angle;
-
-        Vector3 fpVector = Camera.main.WorldToScreenPoint(firePointRotate.position);
-        fpVector = Input.mousePosition - fpVector;
+        Vector3 fpVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        fpVector -= pivot.transform.position;
         float angle = Mathf.Atan2(fpVector.y, fpVector.x) * Mathf.Rad2Deg;
-
-        pivot.position = firePointRotate.position;
-        pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-
-
+        pivot.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
