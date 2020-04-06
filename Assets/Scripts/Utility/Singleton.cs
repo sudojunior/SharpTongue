076880fc5db6389ba780f69886bc767b2a1c715e
Junior <MP232027@falmouth.ac.uk>
2020-04-06 11:49:29 +0100

    Add Singleton class to environment

---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+// ref from http://answers.unity.com/answers/1426123/view.html
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<T>();
+            if (instance == null)
+                Debug.LogError($"Singleton<{typeof(T)}> instance has not been found.");
+            return instance;
+        }
+    }
+
+    protected void Awake()
+    {
+        if (instance == null)
+            instance = this as T;
+        else if (instance != this)
+            DestroySelf();
+    }
+
+    protected void OnValidate()
+    {
+        if (instance == null)
+            instance = this as T;
+        else if (instance != this)
+        {
+            Debug.LogError($"Singleton<{this.GetType()}> already has an instance on scene. Component will be destroyed.");
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.delayCall -= DestroySelf;
+            UnityEditor.EditorApplication.delayCall += DestroySelf;
+#endif
+        }
+    }
+
+    private void DestroySelf()
+    {
+        if (Application.isPlaying)
+            Destroy(this);
+        else
+            DestroyImmediate(this);
+    }
+}
